acb4a060d22c6963a60f83ec2b99185d415e2a93
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2022-10-26 08:16:05 +0700

    first commit

---
+++ resources/views/layouts/user/footer.blade.php
@@ -0,0 +1,48 @@
+<footer id="footer">
+
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 col-md-6 footer-info">
+            <h4>PANTI ASUHAN PUTRI AISYIYAH MALANG</h4>
+            <p>Jl. Mayjen. Mt Haryono Gang III No. 231 A, Dinoyo, Kec. Lowokwaru Malang - Jawa Timur</p>
+            
+              <p>
+               <br>
+               <br>
+               <li><a href="#">Situs </a></li>
+                <strong>Phone:</strong> 082*********<br>
+                <strong>Email:</strong> Email Panti<br>
+              </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Kontak Kami</h4><br><br>
+            <div class="social-links mt-3">
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a><br>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a><br>
+              <a href="#" class="linkedin"><i class="bx bxl-youtube"></i></a><br>
+            </div>
+          </div>
+
+          
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Kelompok 2</span></strong> TI 3E POLINEMA
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/anyar-free-multipurpose-one-page-bootstrap-theme/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
